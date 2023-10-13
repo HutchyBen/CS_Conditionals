643a9dc052cd71d8fdb2a9f994cabd4baaa8f2bf
Ben Hutchinson <benhutchy123456@gmail.com>
2023-10-13 11:43:59 +0100

    Add the older age checker

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -6,7 +6,35 @@ namespace Conditionals
     {
         static void Main()
         {
-            
+            Console.WriteLine("First sibling, what is your name?");
+ 
+            string name1 = Console.ReadLine();
+ 
+            Console.WriteLine("Hello " + name1 + ", How old are you?");
+ 
+            int age1 = int.Parse(Console.ReadLine());
+ 
+            Console.WriteLine("Second sibling, what is your name?");
+ 
+            string name2 = Console.ReadLine();
+ 
+            Console.WriteLine("Hello " + name2 + ", How old are you?");
+ 
+            int age2 = int.Parse(Console.ReadLine());   
+
+            if (age1 > age2)
+            {
+                Console.WriteLine(name1 + " is " + (age1 - age2) + " years older than " + name2);
+            }
+            else if (age1 < age2)
+            {
+                Console.WriteLine(name2 + " is " + (age2 - age1) + " years older than " + name1);
+            }
+            else
+            {
+                Console.WriteLine(name1 + " and " + name2 + " are the same age");
+            }
+
         }
     }
 }
