b13a10128f6d8e6db0e5e08ac846624ad7159c20
Ben Hutchinson <benhutchy123456@gmail.com>
2023-10-13 12:02:07 +0100

    Add Parking Sensor

---
+++ ParkingSensors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ParkingSensors
+{
+    class ParkingSensors
+    {
+        static void Main()
+        {
+            Console.WriteLine("Parking Sensors Application");
+ 
+            Console.WriteLine("Is the car in reverse? Press Y for yes or N for no.");
+            string isInReverse = Console.ReadLine();
+            double emptySpaceBehindCar = float.Parse(Console.ReadLine());
+
+            if (isInReverse!.ToUpper() == "Y" && emptySpaceBehindCar <= 1.5f)
+            {
+                Console.WriteLine("beep, beep, beep");
+            }
+
+
+        }
+    }
+}
